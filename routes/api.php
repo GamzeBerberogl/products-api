db60be25b3170a88e33e908b8113be4c28aaff33
Gamze B. <berberoglugamzee@gmail.com>
2024-05-16 16:49:23 +0300

    Added routes

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,12 @@ use App\Http\Controllers\Profile\ChangePasswordController;
 use App\Http\Controllers\User\ChangePasswordController as UserChangePasswordController;
 use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\ProductTypeController;
+use App\Http\Controllers\PriceSignController;
+use App\Http\Controllers\CurrencyTypeController;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,25 +28,31 @@ use App\Http\Controllers\Auth\ResetPasswordController;
 */
 
 Route::prefix('auth')->group(function () {
-    Route::post('login',[LoginController::class, 'login']);
+    Route::post('login', [LoginController::class, 'login'])->name('login');
     Route::post('password/email', [ResetPasswordController::class, 'sendPasswordResetLinkEmail'])->name('password.email');
-	Route::post('password/reset', [ResetPasswordController::class, 'resetPassword'])->name('password.reset');
+    Route::post('password/reset', [ResetPasswordController::class, 'resetPassword'])->name('password.reset');
     Route::post('password/checkToken', [ResetPasswordController::class, 'checkToken'])->name('password.checkToken');
 });
 
-
-Route::group(['middleware' => 'auth:sanctum'], function() {
+Route::middleware('auth:sanctum')->group(function() {
     Route::prefix('auth')->group(function () {
-        Route::post('logout', [LogoutController::class,'logout']);
+        Route::post('logout', [LogoutController::class, 'logout']);
     });
 
     Route::prefix('profile')->group(function () {
-        Route::post('change-password', ChangePasswordController::class);
-        Route::post('change-role', ChangeRoleController::class);
+        Route::post('change-password', [ChangePasswordController::class, 'changePassword']);
+        Route::post('change-role', [ChangeRoleController::class, 'changeRole']);
     });
-    
-    Route::singleton('profile', ProfileController::class)->only(['show','update']);
 
-    Route::post('users/change-password/{user}', UserChangePasswordController::class);
+    Route::resource('profile', ProfileController::class)->only(['show', 'update']);
+
+    Route::post('users/change-password/{user}', [UserChangePasswordController::class, 'changePassword']);
     Route::apiResource('users', UserController::class)->middleware('role:ROLE_ADMIN');
+
+    Route::apiResource('brands', BrandController::class)->except(['destroy', 'show']);
+    Route::apiResource('categories', CategoryController::class)->except(['destroy', 'show']);
+    Route::apiResource('product_types', ProductTypeController::class)->except(['destroy', 'show']);
+    Route::apiResource('price_signs', PriceSignController::class)->except(['destroy', 'show']);
+    Route::apiResource('currency_types', CurrencyTypeController::class)->except(['destroy', 'show']);
+    Route::apiResource('products', ProductController::class)->except(['destroy', 'show']);
 });
